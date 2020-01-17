0231332ecf33db7132c155995f07064f46f28385
Morgaine <morgainedv@gmail.com>
2020-01-17 18:07:46 +0000

    adds music shop sellproduct functionality

--- src/main/java/MusicShop.java
+++ src/main/java/MusicShop.java
@@ -1,2 +1,37 @@
+import behaviour.ISell;
+
+import java.util.ArrayList;
+
 public class MusicShop {
+
+    private ArrayList<ISell> stock;
+    private double till;
+
+
+    public MusicShop(double till) {
+        this.stock = new ArrayList<ISell>();
+        this.till = till;
+    }
+
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+
+    public double getTill(){
+        return till;
+    }
+
+    public void addToStock(ISell product){
+        stock.add(product);
+    }
+
+    public void removeFromStock(ISell product){
+        stock.remove(product);
+    }
+
+    public void sellProduct(ISell product){
+        double profit = product.markUp();
+        till += profit;
+        removeFromStock(product);
+    }
 }
--- src/test/java/MusicShopTest.java
+++ src/test/java/MusicShopTest.java
@@ -1,2 +1,61 @@
+import instruments.Guitar;
+import instruments.Piano;
+import nonInstruments.MusicBook;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class MusicShopTest {
+
+    MusicShop musicShop;
+    ArrayList stock;
+    Guitar guitar;
+    Piano piano;
+    MusicBook musicBook;
+
+
+    @Before
+    public void before(){
+        musicShop = new MusicShop(1000.00);
+        guitar = new Guitar("wood", "black", "string", 100.00, 200.0, false);
+        piano = new Piano("oak", "white", "percussion", 250.00, 350.00, "Yamaha");
+        musicBook = new MusicBook("Alan Silvestri", "The Music of the Marvel Cinematic Universe", 10.00, 20.00);
+    }
+
+    @Test
+    public void checkStockStartsEmpty(){
+        assertEquals(0, musicShop.getStock().size());
+    }
+
+    @Test
+    public void canGetTill(){
+        assertEquals(1000.00, musicShop.getTill(), 0.01);
+    }
+
+    @Test
+    public void canAddToStock(){
+        musicShop.addToStock(piano);
+        musicShop.addToStock(musicBook);
+        assertEquals(2, musicShop.getStock().size());
+    }
+
+    @Test
+    public void canRemoveFromStock(){
+        musicShop.addToStock(piano);
+        musicShop.addToStock(guitar);
+        musicShop.removeFromStock(guitar);
+        assertEquals(1, musicShop.getStock().size());
+    }
+
+    @Test
+    public void canSellProduct(){
+        musicShop.addToStock(musicBook);
+        musicShop.sellProduct(musicBook);
+        assertEquals(1010.00, musicShop.getTill(), 0.01);
+        assertEquals(0, musicShop.getStock().size());
+    }
+
 }
