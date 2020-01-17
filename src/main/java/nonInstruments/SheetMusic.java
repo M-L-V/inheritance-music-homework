fb91af31098b8b08fea7820edbcca82bbf4a08ed
Morgaine <morgainedv@gmail.com>
2020-01-17 17:30:52 +0000

    adds sheet music

--- src/main/java/nonInstruments/SheetMusic.java
+++ src/main/java/nonInstruments/SheetMusic.java
@@ -1,4 +1,38 @@
 package nonInstruments;
 
-public class SheetMusic {
+import behaviour.ISell;
+
+public class SheetMusic implements ISell {
+
+    private String piece;
+    private String artist;
+    private double buyingPrice;
+    private double sellingPrice;
+
+    public SheetMusic(String piece, String artist, double buyingPrice, double sellingPrice) {
+        this.piece = piece;
+        this.artist = artist;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public String getPiece() {
+        return piece;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public double getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public double markUp(){
+        return sellingPrice -= buyingPrice;
+    }
 }
--- src/test/java/SheetMusicTest.java
+++ src/test/java/SheetMusicTest.java
@@ -1,2 +1,41 @@
+import nonInstruments.SheetMusic;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SheetMusicTest {
+
+    SheetMusic sheetMusic;
+
+    @Before
+    public void before(){
+        sheetMusic = new SheetMusic("Mr Blue Sky", "Electric Light Orchestra", 5.50, 10.50);
+    }
+
+    @Test
+    public void hasPiece(){
+        assertEquals("Mr Blue Sky", sheetMusic.getPiece());
+    }
+
+    @Test
+    public void hasTitle(){
+        assertEquals("Electric Light Orchestra", sheetMusic.getArtist());
+    }
+
+    @Test
+    public void hasBuyingPrice(){
+        assertEquals(5.50, sheetMusic.getBuyingPrice(), 0.01);
+    }
+
+    @Test
+    public void hasSellingPrice(){
+        assertEquals(10.50, sheetMusic.getSellingPrice(), 0.01);
+    }
+
+    @Test
+    public void hasMarkUp(){
+        assertEquals(5.00, sheetMusic.markUp(), 0.01);
+    }
+
 }
