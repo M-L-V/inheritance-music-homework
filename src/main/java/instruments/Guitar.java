c4c39007092846023addcbd241ad681a358df060
Morgaine <morgainedv@gmail.com>
2020-01-17 17:07:39 +0000

    adds guitar set up

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -1,4 +1,19 @@
 package instruments;
 
-public class Guitar {
+public class Guitar extends Instrument {
+
+    Boolean electric;
+
+    public Guitar(String material, String colour, String type, double buyingPrice, double sellingPrice, Boolean electric) {
+        super(material, colour, type, buyingPrice, sellingPrice);
+        this.electric = electric;
+    }
+
+    public Boolean getElectric() {
+        return electric;
+    }
+
+    public String play(){
+        return "Smoke on the water...";
+    }
 }
--- src/test/java/GuitarTest.java
+++ src/test/java/GuitarTest.java
@@ -1,2 +1,50 @@
+import instruments.Guitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("metal", "purple", "string", 100.00, 200.00, true);
+    }
+
+    @Test
+    public void hasMaterial(){
+        assertEquals("metal", guitar.getMaterial());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("purple", guitar.getColour());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("string", guitar.getType());
+    }
+
+    @Test
+    public void hasBuyingPrice(){
+        assertEquals(100.00, guitar.getBuyingPrice(), 0.01);
+    }
+
+    @Test
+    public void hasSellingPrice(){
+        assertEquals(200.00, guitar.getSellingPrice(), 0.01);
+    }
+
+    @Test
+    public void hasElectricStatus(){
+        assertEquals(true, guitar.getElectric());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("Smoke on the water...", guitar.play());
+    }
 }
