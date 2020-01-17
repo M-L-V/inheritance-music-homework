a2516f81a74f7777b6062e13cec3a69254fde99e
Morgaine <morgainedv@gmail.com>
2020-01-17 16:34:33 +0000

    adds abstract class instrument set up

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -1,4 +1,41 @@
 package instruments;
 
-public class Instrument {
+public abstract class Instrument {
+
+    private String material;
+    private String colour;
+    private String type;
+    private double buyingPrice;
+    private double sellingPrice;
+
+    public Instrument(String material, String colour, String type, double buyingPrice, double sellingPrice) {
+        this.material = material;
+        this.colour = colour;
+        this.type = type;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+
 }
+
